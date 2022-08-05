b2b0f9316d2072964b4e2c46e7f318e971e041a0
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-05 21:59:51 +0900

    Create solution.py

---
+++ LEVEL1/소수 만들기/solution.py
@@ -0,0 +1,34 @@
+# combinations 패키지와 2중 for문 사용
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    a = list(combinations(nums, 3))
+    
+    for i in range(len(a)):
+        for j in range(2, sum(a[i]) - 1):
+            if sum(a[i]) % j == 0:
+                break
+        else:
+            answer += 1
+
+    return answer
+  
+'''
+# 3중 for문 사용
+def solution(nums):
+    answer = 0
+    
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                sum = nums[i] + nums[j] + nums[k]
+                
+                for l in range(2, sum - 1):
+                    if sum % l == 0:
+                        break
+                else:
+                    answer += 1        
+    
+    return answer
+'''
