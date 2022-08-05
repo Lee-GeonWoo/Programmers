d8991be62126b9df9c353cc6b66d5198db5a9ea6
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-05 21:09:50 +0900

    Update solution.py

--- LEVEL1/없는 숫자 더하기/solution.py
+++ LEVEL1/없는 숫자 더하기/solution.py
@@ -1,2 +1,13 @@
 def solution(numbers):
     return 45 - sum(numbers)
+
+'''
+# for문을 이용한 방법
+def solution(numbers):
+    answer = 0
+    
+    for i in range(1, 10):
+        if i not in numbers:
+            answer += i
+    return answer
+'''
