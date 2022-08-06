13d616e0849b56ea32be2ab1ec105e602410b244
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-06 17:47:15 +0900

    Update solution.py

--- LEVEL1/3진법 뒤집기/solution.py
+++ LEVEL1/3진법 뒤집기/solution.py
@@ -6,3 +6,22 @@ def solution(n):
         n //= 3
         
     return int(answer, 3)
+
+
+'''
+# 나머지 계산법을 이용한 풀이
+def solution(n):
+    three = []
+    answer = 0
+    
+    while n > 0:
+        three.append(n%3)
+        n //= 3
+    
+    three.reverse()
+    
+    for i in range(len(three)):
+        answer += (three[i] * 3 ** i) 
+    
+    return answer
+'''
