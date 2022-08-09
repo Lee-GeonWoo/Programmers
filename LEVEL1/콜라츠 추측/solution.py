323e499f81d7e2374c7560b9463639c6db3c859c
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-09 18:15:19 +0900

    Create solution.py

---
+++ LEVEL1/콜라츠 추측/solution.py
@@ -0,0 +1,9 @@
+def solution(num):
+    for i in range(500):
+        if num == 1:
+            return 0
+        else:
+            num = num / 2 if num % 2 == 0 else num * 3 + 1
+            if num == 1:
+                return i + 1
+    return -1
