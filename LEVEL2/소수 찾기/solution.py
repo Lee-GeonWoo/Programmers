a74ab0ad3165f5de94cbae01c131f1f7d6cffff0
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-10 16:25:46 +0900

    Create solution.py

---
+++ LEVEL2/소수 찾기/solution.py
@@ -0,0 +1,48 @@
+# 타임 아웃 뜬 코드
+'''
+from itertools import permutations
+
+def solution(numbers):
+    answer = 0
+    a = []
+    
+    for i in range(1, len(numbers)+1):
+        a.append(list(map(''.join, permutations(numbers, i))))
+    a = sum(a, [])
+    a = list(set(map(int, a)))
+
+    for num in a:
+        prime = [0, 0] + [1] * (max(a) - 1)
+        
+        for i in range(2, num+1):
+            for j in range(i*2, num+1, i):
+                prime[j] = 0
+        if prime[num] == 1:
+            answer += 1
+        
+    return answer
+'''
+
+# 다른 사람의 코드
+
+from itertools import permutations
+
+def solution(numbers):
+    answer = 0
+    a = []
+    
+    for i in range(1, len(numbers)+1):
+        a.append(list(map(''.join, permutations(numbers, i))))
+    a = sum(a, [])
+    a = list(set(map(int, a)))
+
+    for num in a:
+        if num == 2:
+            answer += 1
+
+        for j in range(2, num):
+            if num % j == 0:
+                break
+            if j == num-1:
+                answer += 1
+    return answer
